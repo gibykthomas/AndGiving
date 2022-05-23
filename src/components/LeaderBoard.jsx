dbc70f3a852667d4b94faa4c2fe14dee8953a4bd
Giby Thomas <gibythomas@ph0x09vthd.home>
2022-05-23 14:15:31 +0100

    added grid

--- src/components/LeaderBoard.jsx
+++ src/components/LeaderBoard.jsx
@@ -1,7 +1,62 @@
 import {clubData} from '../data/data.js'
+/*import { DataGrid } from '@mui/x-data-grid';
 
 export default function LeaderBoard() {
+    const columns = [
+        { field: "id", headerName: "ID", width: 90 },
+        {
+          field: "club",
+          headerName: "Club",
+          width: 200,
+          /*renderCell: (params) => {
+            return (
+              <div className="userListUser">
+                <img className="userListImg" src={params.row.avatar} alt="" />
+                {params.row.username}
+              </div>
+            );
+          },
+        },
+        { field: "amount_raised", headerName: "Amount Raised", width: 200 },
+        {
+          field: "amount_matched",
+          headerName: "Amount Matched",
+          width: 200,
+        }
+      ];  
   return (
-    <pre>{JSON.stringify(clubData)}</pre>
+    <div className="userList">
+      <DataGrid
+        rows={clubData}
+        columns={columns}
+        pageSize={8}
+      />
+    </div>
   )
+}*/
+import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
+
+const columns: GridColDef[] = [
+  { field: 'id', headerName: 'ID', width: 70 },
+  { field: 'club', headerName: 'Club', width: 130 },
+  { field: 'amount_raised', headerName: 'Amount Raised', width: 130 },
+  {
+    field: 'amount_matched',
+    headerName: 'Amount Matched',
+    type: 'number',
+    width: 90,
+  }
+];
+
+export default function DataTable() {
+  return (
+    <div style={{ height: 400, width: '100%' }}>
+      <DataGrid
+        rows={clubData}
+        columns={columns}
+        pageSize={5}
+        rowsPerPageOptions={[5]}
+      />
+    </div>
+  );
 }
--- src/data/data.js
+++ src/data/data.js
@@ -1,45 +1,53 @@
 export const clubData =[
-    {
+    { "id" : 1,
       "amount_raised": 400,
       "amount_matched": 300,
       "club": "Gladys"
     },
     {
+      "id" : 2,
       "amount_raised": 500,
       "amount_matched": 200,
       "club": "Ada"
     },
     {
+      "id" : 3,
       "amount_raised": 600,
       "amount_matched": 400,
       "club": "Hamilton"
     },
     {
+      "id" : 4,
       "amount_raised": 700,
       "amount_matched": 300,
       "club": "Turing"
     },
     {
+      "id" : 9,
       "amount_raised": 550,
       "amount_matched": 400,
       "club": "Adams"
     },
     {
+      "id" : 5,
       "amount_raised": 400,
       "amount_matched": 300,
       "club": "Aletta"
     },
     {
+      "id" : 6,
       "amount_raised": 450,
       "amount_matched": 350,
       "club": "Almeida"
     },
     {
+      "id" : 7,
       "amount_raised": 800,
       "amount_matched": 500,
       "club": "Dekker"
     },
     {
+      "id" : 8,
       "amount_raised": 1000,
       "amount_matched": 650,
       "club": "Jemison"
