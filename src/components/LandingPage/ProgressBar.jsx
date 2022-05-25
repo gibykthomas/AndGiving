f82ba69d93ebee305b20ecba9cf5c8da7e4a3e55
279Roxana <roxana.c2007@gmail.com>
2022-05-25 11:53:21 +0100

    progress bar changes

--- src/components/LandingPage/ProgressBar.jsx
+++ src/components/LandingPage/ProgressBar.jsx
@@ -2,26 +2,26 @@ import React from "react";
 import './progressBar.css';
 import { clubData } from "../../data/data";
 
-const ProgressBar = ({width}) => {
-    const [value, setValue] = React.useState(0);
-    const ammount = clubData.map(club  => club.amount_raised).reduce((prev, curr) => prev + curr, 0);
-    const percent = ((10000 / ammount) * 100)
-    React.useEffect(() => {
-      setValue(percent);
-    });
+const ProgressBar = ({ width }) => {
+  const [value, setValue] = React.useState(0);
+  const ammount = clubData.map(club => club.amount_raised).reduce((prev, curr) => prev + curr, 0);
+  const percent = ((10000 / ammount) * 100)
+  React.useEffect(() => {
+    setValue(percent);
+  });
 
-    return (
-      <div className="whole-termo">
-        <div className="termo-wrap">
+  return (
+    <div className="whole-termo">
+      <div className="termo-wrap">
 
-          <div className="progress-div" style={{ width: width }}>
-            <div style={{ width: `${value}px` }} className="progress"><span className="raised-Label">Raised so far</span> <span className="ammount-raised">£{ammount}</span></div>
-
-          </div>
+        <div className="progress-div" style={{ width: width }}>
+          <div style={{ width: `${value}px` }} className="progress"><span className="raised-Label">Raised so far</span> <span className="ammount-raised">£{ammount}</span></div>
 
         </div>
-      </div>
-    );
-  };
 
-  export default ProgressBar;
+      </div>
+    </div>
+  );
+};
+
+export default ProgressBar;
