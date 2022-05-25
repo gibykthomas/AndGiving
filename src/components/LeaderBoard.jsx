44c24867c8a12b23c430544ba781028784d5d72e
Giby Thomas <gibythomas@ph0x09vthd.home>
2022-05-25 15:24:40 +0100

    added api response

--- src/components/LeaderBoard.jsx
+++ src/components/LeaderBoard.jsx
@@ -1,35 +1,37 @@
 import "./leaderBoard.css";
-import * as React from 'react'
+import React,{useContext} from "react";
 import {clubData} from '../data/data.js'
 import { DataGrid, GridColDef} from '@mui/x-data-grid';
 import ProgressBar from 'react-bootstrap/ProgressBar'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { DataContext } from '../useContext/DataContext';
 
-const mediumBorder = ["","4px solid gold", "4px solid silver", "4px solid brown"];
+const mediumBorder = ["4px solid gold", "4px solid silver", "4px solid brown"];
 
 
 
 const columns: GridColDef[] = [
   
-  { field: 'club', 
+  { field: 'Club', 
     headerName: 'Club', 
     headerClassName:'gridHeader',
-    width: 200,
+    flex: 3,
     renderCell: (params) => {
+      const id = params.row.id + 1
         return (
 
          <>
-         <div className= "rankData">{params.row.id}  </div> 
+         <div className= "rankData">{id}  </div> 
           <div className="userListUser">
               <img style={{
               border:
-              params.row.id <= 3 ? mediumBorder[params.row.id] : ""
+              params.row.id < 3 ? mediumBorder[params.row.id] : ""
             }} className="userListImg" src={params.row.image} alt="" />
-            {params.row.club}
+            {params.row.Club}
           </div>
           </>
         ); }},
-  { field: 'amount_raised', 
+  { field: 'Total_Raised_to_date', 
     headerName: 'Amount Raised', 
     headerClassName:'gridHeader',
     //width: 400,
@@ -38,16 +40,17 @@ const columns: GridColDef[] = [
  },
  
  {
-    field: 'amount_matched',
+    field: 'Match_requested',
     headerName: 'Amount Matched',
     headerClassName:'gridHeader',
     //width: 200,
     flex: 3,
     renderCell: (params) => {
+        console.log(params)
         return (
           <div className="amountRaised">
-              {params.row.amount_matched}
-              <ProgressBar variant={params.row.amount_matched < 650 ? "warning":"info"} now={params.row.amount_matched/6.5} label={`${Math.trunc(params.row.amount_matched/6.5)}%`}/>
+              {params.row.Match_requested}
+              <ProgressBar variant={params.row.Match_requested < 650 ? "warning":"info"} now={params.row.Match_requested/6.5} label={`${Math.trunc(params.row.Match_requested/6.5)}%`}/>
           </div>
         ); }
     
@@ -60,7 +63,7 @@ const columns: GridColDef[] = [
     renderCell: (params) => {
         return (
           <div className="amountRaised">
-              <PopulateBadge val={params.row.amount_raised }/>
+              <PopulateBadge val={params.row.Total_Raised_to_date }/>
           </div>
         ); }
     
@@ -100,24 +103,29 @@ function PopulateBadge(prop) {
 }
 
 export default function DataTable() {
-
+    const {data, setData}= useContext(DataContext)
+    const sortData = data.sort((a,b) => b.Total_Raised_to_date - a.Total_Raised_to_date)
+    const indexeData = sortData.map((item, id) => Object.assign(item, { id }))
+    console.log(indexeData)
     const getRowSpacing = React.useCallback((params: GridRowSpacingParams) => {
         return {
           top: params.isFirstVisible ? 0 : 5,
           bottom: params.isLastVisible ? 0 : 5,
         };
       }, []);  
-  
+      
   return (
+    
     <div className="dataGrid">
       <h3 className="leaderHeading">Leaderboard</h3>
       <DataGrid
-        rows={clubData}
+        rows={data}
         columns={columns}
         pageSize={7}
         disableColumnSelector = {true}
         rowsPerPageOptions={[7]}
         getRowSpacing={getRowSpacing}
+        getRowId={(row) => row.Club}
         rowSpacingType="border"
         sx={{ '& .MuiDataGrid-row': { borderTopColor: "lightgray", borderTopStyle: 'solid' } }}
 
