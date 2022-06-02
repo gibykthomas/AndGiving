0a06c4c340b68b1f7f9c4fdf53c76c472a1bb99b
279Roxana <roxana.c2007@gmail.com>
2022-06-02 19:44:59 +0100

    info card add styling and responsive

--- src/components/LandingPage/InfoCard/InfoCard.jsx
+++ src/components/LandingPage/InfoCard/InfoCard.jsx
@@ -1,16 +1,15 @@
-import React,{useContext} from 'react'
+import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './landingPage.css'
-import ProgressBar from './ProgressBar';
+import './infoCard.css'
+import ProgressBar from '../InfoCard/ProgrssBar/ProgressBar.jsx';
 import { NavLink } from 'react-router-dom';
-import { DataContext } from '../../useContext/DataContext';
 
 
 const InfoCard = () => {
-    const {data, setData}= useContext(DataContext)
 
     return (
-        <div className="infoCard">
+        
+        <div className="info-card col-md-7 col-lg-6 col-xl-6">
             <div className='top'>
                 <h1 className='title'>242 <br></br>
                     Fundraising<br></br>
@@ -18,7 +17,7 @@ const InfoCard = () => {
                 <p>Vulputate eu scelerisque felis imperdiet proin fermentum leo. Est lorem ipsum dolor sit. </p>
 
                 <NavLink className="nav-link" to="/leaderboard">
-                    <button type="button" className="btn btn-light learn">VIEW LEADERBOARD</button>
+                    <button type="button" className="btn btn-light to-leaderboard">VIEW LEADERBOARD</button>
 
                 </NavLink>
 
